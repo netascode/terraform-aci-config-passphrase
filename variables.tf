6dfa224fa8f6fc5732f83695e5ce4ba61784db69
Balazs Novak-Bohak <balnovak@cisco.com>
2023-02-14 09:11:10 +0100

    Made the config passphrase var sensitive
    This tag hides a variable from terraform command outputs and logs.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "config_passphrase" {
   description = "Config passphrase."
   type        = string
+  sensitive   = true
 
   validation {
     condition     = can(regex("^.{0,32}$", var.config_passphrase))
